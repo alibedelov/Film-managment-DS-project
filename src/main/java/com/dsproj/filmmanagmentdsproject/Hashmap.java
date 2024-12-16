77501d786f2fe6e6046551f752f576f492c175c5
alibedelov <alibedelov@gmail.com>
2024-12-16 13:32:56 +0400

    new code added

--- src/main/java/com/dsproj/filmmanagmentdsproject/Hashmap.java
+++ src/main/java/com/dsproj/filmmanagmentdsproject/Hashmap.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.dsproj.filmmanagmentdsproject;
 
 /**
  *
  * @author Ali
  */
-public class FilmManagmentDSProject {
-
-    public static void main(String[] args) {
-        System.out.println("Salam");
-    }
+public class Hashmap {
+    
 }
