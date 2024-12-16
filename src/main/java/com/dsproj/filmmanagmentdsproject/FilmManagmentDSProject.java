5a176176841922268907b9993cbc443cd3b6d66b
alibedelov <alibedelov@gmail.com>
2024-12-16 13:43:55 +0400

    Update FilmManagmentDSProject.java

--- src/main/java/com/dsproj/filmmanagmentdsproject/FilmManagmentDSProject.java
+++ src/main/java/com/dsproj/filmmanagmentdsproject/FilmManagmentDSProject.java
@@ -11,6 +11,6 @@ package com.dsproj.filmmanagmentdsproject;
 public class FilmManagmentDSProject {
 
     public static void main(String[] args) {
-        System.out.println("Salam");
+        
     }
 }
