7ce65bcf52bec95e5d2ec39a0f04bddc2513dbfe
FatmaAlsaghir <fatmaalsaghir3@gmail.com>
2024-12-24 17:16:27 +0300

    Update BSTNode.java

--- src/main/java/com/dsproj/filmmanagmentdsproject/BSTNode.java
+++ src/main/java/com/dsproj/filmmanagmentdsproject/BSTNode.java
@@ -6,16 +6,73 @@ package com.dsproj.filmmanagmentdsproject;
 
 /**
  *
- * @author DELL
- */
-public class BSTNode {
-    Film film;
-    BSTNode left, right;
+ * @author Ftama Alsaghir 220315105
+ * @param <T>
+public class BST<T extends Comparable<T>> {
+    private static class Node<T> {
+        T data;
+        Node<T> left;
+        Node<T> right;
 
-    public BSTNode(Film film) {
-        this.film = film;
-        this.left = null;
-        this.right = null;
+        public Node(T data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node<T> root;
+
+    public BST() {
+        this.root = null;
+    }
+
+    // Insert method to insert films or actors into the tree
+    public void insert(T data) {
+        root = insertRecursive(root, data);
+    }
+
+    private Node<T> insertRecursive(Node<T> node, T data) {
+        if (node == null) {
+            return new Node<>(data);
+        }
+        if (data.compareTo(node.data) < 0) {
+            node.left = insertRecursive(node.left, data);
+        } else if (data.compareTo(node.data) > 0) {
+            node.right = insertRecursive(node.right, data);
+        }
+        return node;
+    }
+
+    // Search method to search for films or actors by name
+    public boolean search(T data) {
+        return searchRecursive(root, data);
+    }
+
+    private boolean searchRecursive(Node<T> node, T data) {
+        if (node == null) {
+            return false;
+        }
+        if (data.compareTo(node.data) == 0) {
+            return true;
+        } else if (data.compareTo(node.data) < 0) {
+            return searchRecursive(node.left, data);
+        } else {
+            return searchRecursive(node.right, data);
+        }
+    }
+
+    // Display the films or actors in sorted order (in-order traversal)
+    public void display() {
+        displayInOrder(root);
+    }
+
+    private void displayInOrder(Node<T> node) {
+        if (node != null) {
+            displayInOrder(node.left);
+            System.out.println(node.data);  // Print the data of each node
+            displayInOrder(node.right);
+        }
     }
 }
 
